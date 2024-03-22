2a885e5ace2b05654c8ac2e1c728e075e88ab191
Gatoder <trongphuc.phg2201@gmail.com>
2024-03-22 14:30:43 +0700

    complete

---
+++ WindowsFormChess/MainMenu.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace Sakk_Alkalmazás_2._0
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start_btn = new System.Windows.Forms.Button();
+            this.Connection_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Start_btn
+            // 
+            this.Start_btn.BackColor = System.Drawing.Color.Pink;
+            this.Start_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start_btn.Location = new System.Drawing.Point(77, 65);
+            this.Start_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Start_btn.Name = "Start_btn";
+            this.Start_btn.Size = new System.Drawing.Size(176, 66);
+            this.Start_btn.TabIndex = 0;
+            this.Start_btn.Text = "Single Game";
+            this.Start_btn.UseVisualStyleBackColor = false;
+            this.Start_btn.Click += new System.EventHandler(this.Start_btn_Click);
+            // 
+            // Connection_btn
+            // 
+            this.Connection_btn.BackColor = System.Drawing.Color.SlateBlue;
+            this.Connection_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Connection_btn.Location = new System.Drawing.Point(77, 160);
+            this.Connection_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Connection_btn.Name = "Connection_btn";
+            this.Connection_btn.Size = new System.Drawing.Size(176, 64);
+            this.Connection_btn.TabIndex = 1;
+            this.Connection_btn.Text = "Lan Game";
+            this.Connection_btn.UseVisualStyleBackColor = false;
+            this.Connection_btn.Click += new System.EventHandler(this.Connection_btn_Click);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(341, 332);
+            this.Controls.Add(this.Connection_btn);
+            this.Controls.Add(this.Start_btn);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "MainMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "MainMenu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start_btn;
+        private System.Windows.Forms.Button Connection_btn;
+    }
+}
